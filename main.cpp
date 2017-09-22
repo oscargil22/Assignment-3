9f3361254ab1a192f578f9ec85b77b423b129a6f
oscargil22 <31485468+oscargil22@users.noreply.github.com>
2017-09-22 11:34:03 -0700

    Assignment 8

--- main.cpp
+++ main.cpp
@@ -1,49 +1,43 @@
-//
-//  Oscar Gil
-//  Assignment 5
-//
+// Oscar Gil
+// Assignment 8
+// Sep 20, 2017
+
 
 #include <iostream>
 using namespace std;
 
 int main(){
+    int numInput;   //total number that will be searched through
+    int numGuess;   //users guess
+    int count = 0;  //will count how many prime numbers are found
     
-    int num;
-    char letchoice;
+    //Direct user to enter a number and guess
+    cout << "Please enter a number: ";
+    cin >> numInput;
+    cout << "Guess how many prime numbers there are:  ";
+    cin >> numGuess;
     
-    cout<< "If you would like to quit enter 'Q'. If you would like to continue enter any letter: ";
-    cin >> letchoice;
-    
-    
-    if( letchoice == 'q'|| letchoice =='Q'){
-        return 0;
-    }
-
-    
-    while(letchoice != 'q' || letchoice != 'Q')
-    {
-    cout << "Please enter a number to be determined if it's positive or negative: ";
-    cin >> num;
-
-   
-    if ( (num % 2) == 0){
-        cout <<"The number '"<<num<<"' is even." <<endl <<endl;
-        
-    }
-    else (cout << "The number '"<<num<<"' is odd." <<endl <<endl);
-        
-    
-        cout<< "If you would like to quit enter 'Q'. If you would like to continue enter any letter: ";
-        cin >> letchoice;
-
-        if( letchoice == 'q'|| letchoice =='Q'){
-            return 0;
+    //Loop that will find prime numbers
+    for (int i=2; i<numInput; i++)  // will run through until input reached
+        for (int j=2; j<i; j++)     // will eliminate already found prime numbers with
+        {
+            if (i % j == 0)         // if mod == 0 then not prime
+                break;
+            else if (i == j+1){     //
+                count++;            //keeps running count of the prime numbers counted
+                cout << i << " ";   //Displays what the prime numbers are.
+            }
         }
-        
+    
+    // Output that the guess was right
+    if (count == numGuess){
+        cout <<endl << "You are correct there are " <<count <<" prime numbers." <<endl;
     }
-   
     
+    //Output that the guess was wrong
+    else if (count != numGuess){
+        cout <<endl << "You are incorrect there are " <<count <<" prime numbers." <<endl;
+    }
     
-
     return 0;
 }
